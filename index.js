6946ff4ce7a44f77a67e5c72c1335b3fcd7566be
kharchenkoanastasiia <kharchenko.anastasiia@gmail.com>
2018-01-03 13:50:32 +0200

    change ec definition

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ var bs58check = require('bs58check')
 var bufferEquals = require('buffer-equals')
 var createHash = require('create-hash')
 var EC = require('elliptic').ec
-var ec = new EC('secp256k1')
-var ecparams = ec.curve
 var BN = require('bn.js')
 var varuint = require('varuint-bitcoin')
+var ec = null
+
+function getEC () {
+  if (ec === null) {
+    ec = new EC('secp256k1')
+  }
+  return ec
+}
 
 function sha256 (b) {
   return createHash('sha256').update(b).digest()
@@ -49,7 +55,8 @@ function magicHash (message, messagePrefix) {
 
 function sign (message, privateKey, compressed, messagePrefix) {
   var hash = magicHash(message, messagePrefix)
-  var sigObj = ec.sign(hash, privateKey, {canonical: true})
+  var eccrypto = getEC()
+  var sigObj = eccrypto.sign(hash, privateKey, {canonical: true})
   var signature = Buffer.concat([sigObj.r.toArrayLike(Buffer, 'be', 32), sigObj.s.toArrayLike(Buffer, 'be', 32)])
   return encodeSignature(signature, sigObj.recoveryParam, compressed)
 }
@@ -57,14 +64,15 @@ function sign (message, privateKey, compressed, messagePrefix) {
 function recover (message, signature, recovery, compressed) {
   var sigObj = { r: signature.slice(0, 32), s: signature.slice(32, 64) }
 
+  var eccrypto = getEC()
   var sigr = new BN(sigObj.r)
   var sigs = new BN(sigObj.s)
-  if (sigr.cmp(ecparams.n) >= 0 || sigs.cmp(ecparams.n) >= 0) throw new Error("couldn't parse signature")
+  if (sigr.cmp(eccrypto.curve.n) >= 0 || sigs.cmp(eccrypto.curve.n) >= 0) throw new Error("couldn't parse signature")
 
   try {
     if (sigr.isZero() || sigs.isZero()) throw new Error()
 
-    var point = ec.recoverPubKey(message, sigObj, recovery)
+    var point = eccrypto.recoverPubKey(message, sigObj, recovery)
     return Buffer.from(point.encode(true, compressed))
   } catch (err) {
     throw new Error("couldn't recover public key from signature")
